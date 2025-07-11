d74f7c2a89e7d0febf330c4541010a7d9f680fe5
SupriseB <suprisebaloyi17@gmail.com>
2025-07-11 10:18:12 +0200

    Create settings.py

---
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -0,0 +1,12 @@
+# settings.py
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_database_name',
+        'USER': 'your_mysql_user',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
