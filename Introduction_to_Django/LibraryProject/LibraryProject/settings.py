ad88d8e3d09f998a0a9c8c17beb29261317ed18b
SupriseB <suprisebaloyi17@gmail.com>
2025-07-11 14:44:23 +0200

    Update settings.py

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book_store.apps.BookStoreConfig',
+    'bookshelf.apps.BookshelfConfig',
 ]
 
 MIDDLEWARE = [
