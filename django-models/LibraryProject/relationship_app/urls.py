fcdf489debb2a63cb1ebe1b225a628bda8ee77dc
SupriseB <suprisebaloyi17@gmail.com>
2025-07-19 21:10:47 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,10 +10,15 @@ urlpatterns = [
     path('register/', views.register, name='register'),  
     path('admin-role/', admin_view.admin_view, name='admin-role'),
     path('librarian-role/', librarian_view.librarian_view, name='librarian-role'),
-    path('member-role/', member_view.member_view, name='member-role')
+    path('member-role/', member_view.member_view, name='member-role'),
+    path('books/add/', views.add_book_view, name='add-book'),
+    path('books/edit/<int:pk>/', views.edit_book_view, name='edit-book'),
+    path('books/delete/<int:pk>/', views.delete_book_view, name='delete-book'),
     
     # Other views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
 ]
+
+
 
