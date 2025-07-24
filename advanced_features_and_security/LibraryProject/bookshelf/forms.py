c1d9f8d6d93a1e672e6d50e3c671de45e2817e38
SupriseB <suprisebaloyi17@gmail.com>
2025-07-24 13:18:24 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -7,10 +7,4 @@ from django import forms
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=255)
 
-<!-- templates/books/book_form.html -->
 
-<form method="POST">
-    {% csrf_token %}
-    {{ form.as_p }}
-    <button type="submit">Save</button>
-</form>
