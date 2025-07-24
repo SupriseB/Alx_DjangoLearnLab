0ead477460fb07f416766bd66a2aed1169b5933a
SupriseB <suprisebaloyi17@gmail.com>
2025-07-24 13:01:43 +0200

    Create forms.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,16 @@
+# forms.py
+
+from django import forms
+
+class SearchForm(forms.Form):
+    title = forms.CharField(max_length=255)
+
+
+
+<!-- templates/books/book_form.html -->
+
+<form method="POST">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <button type="submit">Save</button>
+</form>
