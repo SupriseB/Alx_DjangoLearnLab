c30f5c9efcac575b431af0b84dbb17a338a5bf85
SupriseB <suprisebaloyi17@gmail.com>
2025-07-24 13:03:10 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,11 +1,11 @@
 # forms.py
 
+ExampleForm
+
 from django import forms
 
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=255)
-
-
 
 <!-- templates/books/book_form.html -->
 
