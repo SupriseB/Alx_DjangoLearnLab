fbfa2177c39676911cd37aa03356dc174c162fd0
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 20:06:21 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,11 @@
-# blog/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Blog
+
+# --------------------------
+# User and Profile Forms
+# --------------------------
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -31,3 +34,17 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio', 'avatar']
+
+
+# --------------------------
+# Blog Form (for CRUD posts)
+# --------------------------
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter blog title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your content here...'}),
+        }
