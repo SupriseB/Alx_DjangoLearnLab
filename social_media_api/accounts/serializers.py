5dc705b33568355d872847156b2b2d5b18c96c9b
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 21:17:24 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,26 +1,27 @@
 from rest_framework import serializers
-from .models import CustomUser
-from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
+User = get_user_model()
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True)  # explicit CharField
     token = serializers.SerializerMethodField()
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'token')
 
     def get_token(self, obj):
-        token, _ = Token.objects.get_or_create(user=obj)
-        return token.key
+        # Explicitly create token
+        return Token.objects.create(user=obj).key
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        # Use create_user method instead of manually setting password
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
         )
-        user.set_password(validated_data['password'])
-        user.save()
         return user
