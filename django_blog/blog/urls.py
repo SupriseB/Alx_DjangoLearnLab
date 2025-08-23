3c658cb4092715a54f54e55e369f4366ae27cff2
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 20:25:51 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,6 +8,7 @@ from .views import BlogLoginView, BlogLogoutView, register, profile
 
 from django.urls import path
 from .views import BlogLoginView, BlogLogoutView, register, profile
+from . import views
 from .views import (
     PostListView,
     PostDetailView,
@@ -21,12 +22,16 @@ urlpatterns = [
     path('logout/', BlogLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
-     path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/new/", PostCreateView.as_view(), name="post-create"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/")
+    path('posts/<int:post_id>/comments/new/', views.add_comment, name='add_comment'),
+    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+
 ]
 
 
