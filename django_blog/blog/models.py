9145be738f8a85f9a40cf97988149f4805143d4e
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 20:17:27 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from django.db import models
+from django.contrib.auth.models import User
 
 """
     Represents an author in the blog system.
@@ -50,3 +52,15 @@ class Blog(models.Model):
     def get_absolute_url(self):
         # this allows Django to redirect to the detail page after create/update
         return reverse("blog-detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+
