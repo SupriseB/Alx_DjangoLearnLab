cfe1009dafaffbaac3c5c46fbf2750be16681e6b
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 20:09:53 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,10 +8,25 @@ from .views import BlogLoginView, BlogLogoutView, register, profile
 
 from django.urls import path
 from .views import BlogLoginView, BlogLogoutView, register, profile
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path('login/', BlogLoginView.as_view(), name='login'),
     path('logout/', BlogLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
+     path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-edit"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
+
+
+
