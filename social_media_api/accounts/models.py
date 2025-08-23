79ce9a5fad6b624f6028fada75a52043cc59f914
SupriseB <suprisebaloyi17@gmail.com>
2025-08-23 21:43:05 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,6 +8,10 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers_users', blank=True)
 
     def __str__(self):
         return self.username
+
+
+
